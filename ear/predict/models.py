73b5bc4ca8ced707e122f5009313f46f3b584f28
jihwan9675 <jihwan9675@gmail.com>
2021-04-20 19:24:02 +0900

    models.py 리팩토링

--- ear/predict/models.py
+++ ear/predict/models.py
@@ -4,13 +4,15 @@ from user.models import User
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 
+
 class Predict(models.Model):
-    user = models.ForeignKey('user.User', on_delete=models.CASCADE, verbose_name='사용자')
+    user = models.ForeignKey(
+        'user.User', on_delete=models.CASCADE, verbose_name='사용자')
     result = models.CharField(max_length=32, verbose_name='진단결과')
     predict_date = models.DateTimeField(auto_now_add=True, verbose_name='진단날짜')
     image = models.ImageField(null=True)
     count = models.IntegerField()
-    accuracy = models.DecimalField(max_digits = 100, decimal_places = 3)
+    accuracy = models.DecimalField(max_digits=100, decimal_places=3)
 
     def __str__(self):
         return str(self.user) + ' ' + str(self.result)
@@ -20,8 +22,9 @@ class Predict(models.Model):
         verbose_name = '진단'
         verbose_name_plural = '진단'
 
+
 class UploadFileModel(models.Model):
     file = models.FileField(null=True)
-    
+
     def __str__(self):
         return str(self.file)
