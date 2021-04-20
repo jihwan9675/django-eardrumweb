c37f9f963d13f46b725575d4a6467d4c965223f1
jihwan9675 <jihwan9675@gmail.com>
2021-04-20 19:23:07 +0900

    admin.py 리팩토링

--- ear/predict/admin.py
+++ ear/predict/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from .models import UploadFileModel, Predict
-# Register your models here.
+
+
 class PredictAdmin(admin.ModelAdmin):
-    list_display = ('user','result','predict_date','image','count','accuracy')
+    list_display = ('user', 'result', 'predict_date',
+                    'image', 'count', 'accuracy')
+
+
 class UploadAdmin(admin.ModelAdmin):
     list_display = ('file',)
 
+
 admin.site.register(UploadFileModel, UploadAdmin)
 admin.site.register(Predict, PredictAdmin)
-
