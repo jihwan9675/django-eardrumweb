1b00c6dd297089a3dfc3916a4ed01bf3b3952157
jihwan9675 <jihwan9675@gmail.com>
2021-04-20 19:21:58 +0900

    forms.py에 주석 변경

--- ear/predict/forms.py
+++ ear/predict/forms.py
@@ -3,7 +3,10 @@ from .models import Predict
 from .models import UploadFileModel
 
 class PredictForm(forms.ModelForm):
-    file = forms.FileField() # image field
+    # Eardrum Image Field
+    # File Name Rule : ('d_' or 'c_' or ' ')id_date_.extention
+    # ex) /media/c_wg0705_2021-04-01-000000.jpg
+    file = forms.FileField() 
 
     def __init__(self, request, *args, **kwargs):
         super(PredictForm, self).__init__(*args, **kwargs)
