461520f4a0709b7c491d863320e0a62c93603327
jihwan9675 <jihwan9675@gmail.com>
2021-04-20 19:31:38 +0900

    models.py 리팩토링

--- ear/user/models.py
+++ ear/user/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 
+
 class User(models.Model):
     userid = models.CharField(max_length=64,
-                                verbose_name='아이디')
+                              verbose_name='아이디')
     username = models.CharField(max_length=64,
                                 verbose_name='사용자명')
     password = models.CharField(max_length=64,
                                 verbose_name='비밀번호')
-    level = models.CharField(max_length=8,verbose_name='등급', choices=(
-        ('admin','admin'),
-        ('doctor','doctor'),
-        ('user','user')
-    )) 
+    level = models.CharField(max_length=8, verbose_name='등급', choices=(
+        ('admin', 'admin'),
+        ('doctor', 'doctor'),
+        ('user', 'user')
+    ))
     registered_dttm = models.DateTimeField(auto_now_add=True,
-                                            verbose_name='등록시간')
+                                           verbose_name='등록시간')
+
     def __str__(self):
         return self.username
 
