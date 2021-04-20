2e2890a65692aa963dbf8d26d1c3821fff8c9829
jihwan9675 <jihwan9675@gmail.com>
2021-04-20 19:23:42 +0900

    app.py 리팩토링

--- ear/predict/apps.py
+++ ear/predict/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
-import os, sys, json, datetime, cv2, skimage.draw
+import os
+import sys
+import json
+import datetime
+import cv2
+import skimage.draw
 import numpy as np
 from mrcnn.visualize import display_instances
 import matplotlib.pyplot as plt
@@ -17,19 +22,19 @@ from tensorflow.python.keras.backend import set_session
 
 
 class predictmodel():
-    eardrum = ["Normal","Traumatic Perforation", "Acute Otitis Media", "Chronic Otitis Media",
+    eardrum = ["Normal", "Traumatic Perforation", "Acute Otitis Media", "Chronic Otitis Media",
                "Congential Cholesteatoma", "Otitis Media with Effusion", "I don't know"]
     sess = tf.compat.v1.Session()
     graph = tf.compat.v1.get_default_graph()
     set_session(sess)
-    directory = "media/" # Image Save path
+    directory = "media/"  # Image Save path
     config = InferenceConfig()
     config.display()
-    model = modellib.MaskRCNN(mode="inference", config=config, model_dir="/logs")
+    model = modellib.MaskRCNN(
+        mode="inference", config=config, model_dir="/logs")
     model.load_weights("static/Weight/mask_rcnn_eardrum_0043.h5", by_name=True)
     classificationModel = build_model(6)
     classificationModel.load_weights("static/Weight/efficientNetB0.h5")
-   
 
     def detect_roi(self, image_path):
         with self.graph.as_default():
@@ -41,47 +46,47 @@ class predictmodel():
             r = self.model.detect([image], verbose=1)[0]
 
             # No instance == dont need classification
-            if r['rois'].shape[0] == 0: 
+            if r['rois'].shape[0] == 0:
                 return r['rois'].shape[0]
 
             # Detected Object's Range --> For Cropping
-            y1, x1, y2, x2 = r['rois'][0] 
-            crop_image=image[y1:y2,x1:x2]
+            y1, x1, y2, x2 = r['rois'][0]
+            crop_image = image[y1:y2, x1:x2]
 
             # Save Image include detection information
-            visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
-                                "Ear", r['scores'], 
-                                title="Predictions",filename="media/"+"d_"+image_path)
+            visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+                                        "Ear", r['scores'],
+                                        title="Predictions", filename="media/"+"d_"+image_path)
             # Save Croped Image
             skimage.io.imsave(self.directory+"c_"+image_path, crop_image)
 
             return r['rois'].shape[0]
 
-
-    def predictEardiease(self,image_path):
+    def predictEardiease(self, image_path):
         with self.graph.as_default():
             set_session(self.sess)
             # Preprocess : Resize and CLAHE
-            img=preprocess(self.directory+"c_"+image_path)
+            img = preprocess(self.directory+"c_"+image_path)
 
             # Predict
             prediction = self.classificationModel.predict(img, verbose=1)
 
             # Find Max value's Index
-            Accuracy=max(prediction[0])
-            idx = np.where(prediction[0]==Accuracy)[0][0]
+            Accuracy = max(prediction[0])
+            idx = np.where(prediction[0] == Accuracy)[0][0]
 
             return idx, Accuracy
 
     def predict(self, image_path):
-        detected_instance_count=self.detect_roi(image_path=image_path)
+        detected_instance_count = self.detect_roi(image_path=image_path)
         idx = 6
         Accuracy = 0
         if detected_instance_count > 0:
             idx, Accuracy = self.predictEardiease(image_path)
-        
+
         # return list ...  [DiseaseName, Count of Detected Instance, Accuracy]
         return [self.eardrum[idx], detected_instance_count, Accuracy]
+
 
 class PredictConfig(AppConfig):
     name = 'predict'
